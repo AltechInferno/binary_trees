f2972a50fd1092f21e7e361798367f0a7d747633
Altech Inferno <altechinferno@gmail.com>
2023-08-30 20:04:09 -0700

    task13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - this counts nodes in a binary tree with children
+ * binary_tree_nodes - Counts nodes in a binary tree with children
  *
- * @tree: Tree to count nodes for
+ * @tree: Tree to count nodes
  * Return: Number of nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
@@ -11,7 +11,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	size_t nodes = (tree->left || tree->right) ? 1 : 0;
+	size_t nodes = 0;
+
+	if (tree->left || tree->right)
+		nodes = 1;
+
 	nodes += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 
 	return (nodes);
